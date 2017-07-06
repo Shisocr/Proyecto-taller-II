6565312208fb2b7bf221fab0c5c29dff20190253
GiacomoViso <giacomo.viso@hotmail.com>
2017-07-06 13:37:18 -0500

    db_connect.php

--- php/DataBase/db_connect.php
+++ php/DataBase/db_connect.php
@@ -1,4 +1,5 @@
 <?php
+//Esta clase hace la coneccion a la base de datos, utilizamos PDO que es la manera estandar de conectar a cualquier base de datos. Con funcion getConection() instaciamos la coneccion de manera mas segura, y retornamos una variable de nombre conection que tiene la coneccion y lista para mandarle sentencias SQL, en la function queryList(a,b) esta funcion espera recibir dos parametros una sentencia SQL y otra un Array de parametro, hace el bindparam para mantener la base de datos segura en todo momento, y retornamos el resultado de la base de datos True/False or NULL.
 
 class PDOconnect{
 
